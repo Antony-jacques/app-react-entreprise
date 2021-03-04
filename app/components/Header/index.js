33f6fb7be0f11d0a9ed1f686a69ed932df1a904b
Antony <antony.jacques@laposte.net>
2021-03-04 17:22:24 +0100

    initial commit

---
+++ app/components/Header/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import A from './A';
+import Img from './Img';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+import Banner from './banner.jpg';
+import messages from './messages';
+
+import Button from '@material-ui/core/Button';
+import { Link, NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <A href="https://www.reactboilerplate.com/">
+        <Img src={Banner} alt="react-boilerplate - Logo" />
+      </A>
+      <NavBar>
+        <NavLink to="/search" style={{textDecoration:'none'}}>
+          <Button variant="contained" color="secondary" >
+            Search
+          </Button>
+        </NavLink>
+        <HeaderLink to="/">
+          <FormattedMessage {...messages.home} />
+        </HeaderLink>
+        <HeaderLink to="/features">
+          <FormattedMessage {...messages.features} />
+        </HeaderLink>
+      </NavBar>
+    </div>
+  );
+}
+
+export default Header;
