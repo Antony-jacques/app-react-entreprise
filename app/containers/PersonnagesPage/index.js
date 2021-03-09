5510a75ba5d12afe9c4538f48d3c7d94bcc324c0
Antony <antony.jacques@laposte.net>
2021-03-09 17:23:13 +0100

    appel API part 1
    L'appel est bien visible dans le network mais rien n'est visible sur la pagge pour l'instant

--- app/containers/PersonnagesPage/index.js
+++ app/containers/PersonnagesPage/index.js
@@ -16,8 +16,44 @@ import Checkbox from '@material-ui/core/Checkbox';
 
 import TablePerso from './TablePerso';
 
+import { useEffect } from 'react';
+
+/*
+
+const handleCallAPIPerso = (name)=>{
+  // on utilise les bactics pour pouvoir variabiliser l'url avec la variable name en utilisant la synthaxe ${name}
+  const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&apikey=168ac8865b5464d4e8ed9e5b2281ba79`;
+  //fait un appel au serveur
+  fetch(url)
+};
+
+*/
+/*
+  useEffect(() => {
+    fetch(url + songId)
+     .then(resp => resp.json())
+     .then(data => this.setSong()
+    }, [selectedSong])
+ */
 
 export default function PersonnagesPage() {
+  /* useEffect(  ()=>{
+    function myAlrt(){
+      alert('fetch hook');
+    }
+*/
+
+const handleCallAPIPerso = (name)=> {
+  // on utilise les bactics pour pouvoir variabiliser l'url avec la variable name en utilisant la synthaxe ${name}
+  const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&apikey=168ac8865b5464d4e8ed9e5b2281ba79`;
+  //fait un appel au serveur
+  fetch(url);
+}
+
+
+//hook qui appelle la fonction responsable de la requete vers l'Api
+  useEffect(handleCallAPIPerso('spider'));
+
   return (
     <div>
       <Helmet>
@@ -30,12 +66,12 @@ export default function PersonnagesPage() {
       <H1>
         <FormattedMessage {...messages.header} />
       </H1>
-      <TextField id="standard-search" label="Search field" type="search"  />
+      <TextField id="standard-search" label="Search field" type="search" />
       <Button variant="contained" color="primary">
         Rechercher
       </Button>
       <Checkbox />
-      <TablePerso></TablePerso>
+      <TablePerso />
     </div>
   );
 }
