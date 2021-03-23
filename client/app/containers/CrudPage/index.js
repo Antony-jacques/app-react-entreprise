03682ca1e0bc9fc15fdbdbe9574fc862d5666cf6
Antony <antony.jacques@laposte.net>
2021-03-23 22:12:53 +0100

    insert + select ok

--- client/app/containers/CrudPage/index.js
+++ client/app/containers/CrudPage/index.js
@@ -12,6 +12,8 @@ import './index.css';
 
 import { useEffect, useState } from 'react';
 
+import Axios from 'axios';
+
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -28,12 +30,31 @@ const useStyles = makeStyles(theme => ({
 export default function CrudPage() {
   const classes = useStyles();
 
+  const [characterName, setCharacterName] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const [characterList, setCharacterList] = useState([]);
+
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response)=>{ // response designe ce qui est récupéré quand on fait appel à l'url
+    setCharacterList(response.data)    })
+  },[])
+
+  // requete post avec Axios
+const submitCharacter = ()=>{
+  Axios.post('http://localhost:3001/api/insert', {characterName:characterName, description: description, image:image}) // on crée un objet auquel on passe les states et qui sera ensuite transmis au back
+  .then(()=>{
+    alert('success insert');
+  })
+}
+
   return (
     <div className="form">
       <h1>Crée ton porpre héros !</h1>
       <form className={classes.root} noValidate autoComplete="off">
         <div>
-          <TextField id="standard-basic" label="Nom" />
+          <TextField id="standard-basic" label="Nom" onChange={(e)=>{setCharacterName(e.target.value)}} />
         </div>
         <div>
           <TextField
@@ -43,6 +64,7 @@ export default function CrudPage() {
             rows={4}
             variant="outlined"
             label="Descripion"
+            onChange={(e)=>{setDescription(e.target.value)}}
           />
         </div>
         <div>
@@ -53,11 +75,16 @@ export default function CrudPage() {
             rows={4}
             variant="outlined"
             label="url de l'image"
+            onChange={(e)=>{setImage(e.target.value)}}
           />
         </div>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={submitCharacter}>
         Envoyer
       </Button>
+
+      {characterList.map((val)=>{
+        return <h1>Nom: {val.nom} | Description: {val.description}</h1>
+      })}
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,13 +1,48 @@
 const express = require('express')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mysql = require('mysql')
 
-
-
-//route
-app.get('/', (req,res)=>{   // require pour recup de la data venant u front / response pour envoyer de la data au front
-    //f qui se declenche avec cette route
-    res.send("hello anto")
+//connexion
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'crudmarveldatabase'
 })
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// on crée une autre route avec la data du back
+app.get('/api/get', (req, res)=>{
+    const sqlSelect = "SELECT * FROM personnages";
+    db.query(sqlSelect,(err, result)=>{
+        //envoyer les infos récupérées au front
+        res.send(result)
+    });
+
+
+})
+
+//route qui est appelée par le front
+app.post('/api/insert', (req,res)=>{   // require pour recup de la data venant u front / response pour envoyer de la data au front
+    //f qui se declenche avec cette route
+
+    // on recupere les variables venant du front qui se trouvent dans req
+    const characterName = req.body.characterName
+    const description = req.body.description
+    const image = req.body.image
+
+
+    // requete sql avec les variables récupérées dans le front 
+    const sqlInsert = "INSERT INTO personnages(nom, description,image) VALUES (?,?,?)"
+    db.query(sqlInsert, [characterName, description, image],(err, result)=>{
+        console.log('result',result )
+    });
+});
 app.listen(3001, ()=>{
     console.log('running on port 3001');
 })
